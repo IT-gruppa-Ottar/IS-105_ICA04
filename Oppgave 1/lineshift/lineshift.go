f13c744d1e8d05c32a0102b432508993d2e294a8
Christoffer Lislevand <chrirl16@student.uia.no>
2017-03-21 16:43:59 +0100

    Overførte koden fra oppgave 3 til oppgave 1b.

--- Oppgave 1/lineshift/lineshift.go
+++ Oppgave 1/lineshift/lineshift.go
@@ -1,58 +1,26 @@
-
-//kode som vi kan bruke for å løse oppgaven
-
 package lineshift
 
 import (
-	"fmt"
-	"strings"
-
+	"os"
+	"log"
+	"bytes"
 )
 
-
-func LineShiftCheck()  {
-
-	//Denne stringen skal egentlig returneres av en fil.
-	// Får ikke til å bruke FileToByteSlice til å returnere de eksakte binære verdiene som en string, og kan derfor ikke bruke strings.contains da den ikke støtter binære verdier
-
-	str := "84 101 115 116 101 114 32 108 105 110 106 101 115 107 105 102 116 46 13 10 79 103 32 101 110 32 116 105 108 32 46 46 46 13 10 79 103 32 101 110 32 116 105 108 32 46 46 46 13 10 79 103 32 101 110 32 116 105 108 32 46 46 46 13 10"
-
-	subStr := "10"
-
-	if strings.Contains(str, subStr) {
-		fmt.Println("Contains binary 10 = contains linefeed")
-	} else {
-		fmt.Println("Does not contain linefeed")
+//noinspection GoUnusedExportedFunction
+func Lineshift(filename string, search string) int{
+	//Åpner filen
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	subStr2 := "13"
+	buffer := make([]byte, 512*1024)
+	//Teller linjeskift
+	counter := 0
+	//Søker på linjeskift
+	searchFor := []byte(search)
 
-	if strings.Contains(str, subStr2) {
-		fmt.Println("Contains binary 13 = contains carraige return")
-	} else {
-		fmt.Println("Does not contain carriage return")
-	}
-
-}
-
-func LineShiftCheck2(filename string)  {
-
-	//Fungerer ikke
-
-	subStr := "\n+"
-
-	if strings.Contains(filename, subStr) {
-		fmt.Println("Contains linefeed")
-	} else {
-		fmt.Println("Does not contain linefeed")
-	}
-
-	subStr2 := "\r+"
-
-	if strings.Contains(filename, subStr2) {
-		fmt.Println("Contains carriage return")
-	} else {
-		fmt.Println("Does not contain carriage return")
-	}
-
+	c, _ := file.Read(buffer)
+	counter += bytes.Count(buffer[:c], searchFor)
+	return counter
 }
--- Oppgave 1/main.go
+++ Oppgave 1/main.go
@@ -1,33 +1,26 @@
 package main
 
 import (
-	"./filutils"
-	"./lineshift"
 	"fmt"
 	"os"
+	"./filutils"
+	"./lineshift"
 )
-
-
 
 func main() {
 	fmt.Println("Oppgave 1a")
-
 	fmt.Println("Text 1")
 	fileutils.FileToByteslice("text1.txt")
 
 	fmt.Println("Text 2")
 	fileutils.FileToByteslice("text2.txt")
 
+	fmt.Println()
 
+	fmt.Println("Oppgave 1b")
 
-	fmt.Println("Oppgave 1b metode 1")
-	lineshift.LineShiftCheck()
-
-	fmt.Println("Oppgave 1b metode 2, denne metoden virker ikke, men tar fil som argument")
 	filename := os.Args[1]
-	lineshift.LineShiftCheck2(filename)
+	amount := lineshift.Lineshift(filename, "\n")
 
-
-
-
+	fmt.Println(amount)
 }
