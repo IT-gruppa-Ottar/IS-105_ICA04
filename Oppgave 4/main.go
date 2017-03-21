0b39c2155834eec5e6b6631eb9b7873ffd1b9350
Christoffer Lislevand <chrirl16@student.uia.no>
2017-03-21 10:28:17 +0100

    Intital commit - Oppgave 4a

---
+++ Oppgave 4/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	printUIA()
+}
+
+
+func printUIA(){
+	//Kolonner
+	fmt.Println("UiAs fakultet \t\t\t\t Antall studenter \t \t Sannsynlighet")
+	fmt.Println("Helse-og idrettsfag \t\t 1 829")
+	fmt.Println("Humaniora og pedagogikk \t 1 525")
+	fmt.Println("Kunstfag \t\t\t\t\t   420")
+	fmt.Println("Teknologi og realfag \t\t 2 166")
+	fmt.Println("Lærerutdanningen \t\t\t 1 506")
+	fmt.Println("Okonomi og samf.vitenskap \t 3 093")
+}
