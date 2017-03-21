620e90ab6208e3ca5d4b4c942456106c45bf5c5b
froleo <frode.leonhardsen@gmail.com>
2017-03-21 21:05:55 +0100

    Revert "Overførte koden fra oppgave 3 til oppgave 1b."
    This reverts commit f13c744d1e8d05c32a0102b432508993d2e294a8.

--- Oppgave 1/main.go
+++ Oppgave 1/main.go
@@ -1,26 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"./filutils"
 	"./lineshift"
+	"fmt"
+	"os"
 )
+
+
 
 func main() {
 	fmt.Println("Oppgave 1a")
+
 	fmt.Println("Text 1")
 	fileutils.FileToByteslice("text1.txt")
 
 	fmt.Println("Text 2")
 	fileutils.FileToByteslice("text2.txt")
 
-	fmt.Println()
 
-	fmt.Println("Oppgave 1b")
 
+	fmt.Println("Oppgave 1b metode 1")
+	lineshift.LineShiftCheck()
+
+	fmt.Println("Oppgave 1b metode 2, denne metoden virker ikke, men tar fil som argument")
 	filename := os.Args[1]
-	amount := lineshift.Lineshift(filename, "\n")
+	lineshift.LineShiftCheck2(filename)
 
-	fmt.Println(amount)
+
+
+
 }
